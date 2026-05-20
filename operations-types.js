be7200d53b263572f204676cc582d42608eeff52
Nikita Muller <nikitamuller1005@gmail.com>
2026-05-20 14:35:27 +0200

    added "operation-types.js"

---
+++ operations-types.js
@@ -0,0 +1,14 @@
+let number1 = 10;
+let number2 = 5;
+let number3 = "nikita";
+
+//converting a number to string
+let number1String = number1.toString();
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("number1 = " + number1)
+console.log("number2 = " + number2)
+console.log("the sum of " + number1 + " + " + number2 + " = " + sum) 
